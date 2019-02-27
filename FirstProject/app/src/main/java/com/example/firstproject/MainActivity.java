310c414309c7c4fbacb930160bba80067345d661
Michal Madera <michal.jan.madera@gmail.com>
2019-02-27 12:24:07 +0100

    first commit

---
+++ FirstProject/app/src/main/java/com/example/firstproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.firstproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //First project
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
